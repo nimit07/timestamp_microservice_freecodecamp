aca50c6c4dd3c77a175482a2f483d49e8f5998df
Nimit <nimitkhandelwal7@gmail.com>
2017-01-18 05:18:02 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+
+//Creating Router() object
+
+
+
+// Provide all routes here, this is for Home page.
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+app.get("/:data",function(req,res){
+  if(isNaN(req.params.data)) {
+    res.json({"unix" : Math.round(new Date(req.params.data).getTime()/1000),"date":req.params.data});    
+  }
+  else{
+    var d=new Date(Number(req.params.data) * 1000);
+    res.json({"unix" : req.params.data,"date":(monthNames[d.getMonth()]+" "+d.getDate()+" "+d.getFullYear())});    
+
+  }
+});
+
+// Tell express to use this router with /api before.
+// You can put just '/' if you don't want any sub path before routes.
+
+
+
+// Listen to this Port
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
+
